32ad2b6b882f2124814f301ed083e90646053047
Lorenzo Appendini <lorenzoappendini@gmail.com>
2020-11-09 15:40:57 +0100

    [PULSBS-35] feat: updated TeacherPage.js and related components

--- client/src/pages/TeacherPage.js
+++ client/src/pages/TeacherPage.js
@@ -4,7 +4,7 @@ import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import API from "../api/Api";
 import APIfake from '../tests/APIfake';
-import {CoursePanel} from '../components/TeacherComp';
+import {CoursePanel,LecturePanel,StudentPanel} from '../components/TeacherComp';
 import InfoPanel from '../components/InfoPanel';
 
 class TeacherPage extends React.Component {
@@ -15,15 +15,40 @@ class TeacherPage extends React.Component {
      */
     constructor(props){
         super(props);
-        this.state = {user : props.user,courses : [],lessons : [],students : []};
+        this.state = {user : props.user,courses : [],lectures : [],students : []};
     }
     /**
      * componentDidMount fetch the all courses of the teacher 
      */
     componentDidMount(){
         //to_do
+        this.getCoursesByTeacherId(this.state.user.userId);
     }
     
+    getCoursesByTeacherId = (teacher) =>{
+        APIfake.getCoursesByTeacherId(teacher)
+        .then((courses)=>{
+            this.setState({courses : courses})
+        })
+        .catch();
+    }
+
+    updateLectures = (courseId) =>{
+        APIfake.getLecturesByCourseIdT(this.state.user.userId,courseId)
+        .then((lectures)=>{
+            this.setState({lectures : lectures})
+        })
+        .catch();
+    }
+
+    updateStudents = (lectureId) =>{
+        APIfake.getStudentsByLectureId(lectureId)
+        .then((students)=>{
+            this.setState({students : students})
+        })
+        .catch();
+    }
+
     render(){
         return(
             <>
@@ -33,15 +58,15 @@ class TeacherPage extends React.Component {
                     <InfoPanel user={this.state.user}/>
                 </Col>
                 <Col sm='8'>
-                    <CoursePanel courses={this.state.courses}/>
+                    <CoursePanel courses={this.state.courses} update={this.updateLectures}/>
                 </Col>
                 </Row>
                 <Row>
                 <Col sm='6'>
-                    
+                    <LecturePanel lectures={this.state.lectures} update={this.updateStudents}/>
                 </Col>
                 <Col sm='6'>
-                    
+                    <StudentPanel students={this.state.students}/>
                 </Col>
                 </Row>
             </Container>
@@ -49,8 +74,5 @@ class TeacherPage extends React.Component {
         );
     }
 }
-
-//<LessonPanel lessons={this.state.lessons}/>
-//<StudentPanel students={this.state.students}/>
 
 export default TeacherPage;
