197c47f437b321266f358dd0046c27999b68c3ea
Lorenzo Appendini <lorenzoappendini@gmail.com>
2020-11-09 21:21:03 +0100

    [PULSBS-35] feat: added visualization features fro tables

--- client/src/components/TeacherComp.js
+++ client/src/components/TeacherComp.js
@@ -2,21 +2,30 @@ import React from 'react';
 import Container from "react-bootstrap/Container";
 import Table from 'react-bootstrap/Table';
 import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 //import API from '../api/Api';
 
 const Checkbox = ({ name, checked = false, onChange }) => (
     <Form.Check name={name} checked={checked} onChange={onChange} />
 );
 
+
 class CoursePanel extends React.Component {
 
     constructor(props){
         super(props);
-        this.state = {};
+        this.state = { currentPage: 0};
     }
 
     handler = (e) =>{
             this.props.update(e.target.name);
+    }
+
+    onClick = (e) =>{
+        if(e.target.name=="prev")
+            this.setState({currentPage: this.state.currentPage-1});
+        else //next
+            this.setState({currentPage: this.state.currentPage+1});
     }
 
     render(){
@@ -33,10 +42,17 @@ class CoursePanel extends React.Component {
                         </thead>
                         <tbody>
                         {
-                            this.props.courses.map((course) => (<CoursePanelRow key={course.courseId} checkedOne={this.props.sCourse} course={course} handler={this.handler}/>))
+                            this.props.courses.map((course) => (
+                            <CoursePanelRow key={course.courseId} checkedOne={this.props.sCourse} course={course} handler={this.handler} pMap={this.props.pageMap} nPages={this.props.nPages} current={this.state.currentPage}/>
+                            ))
                         }
                         </tbody>
                     </Table>
+                    { this.props.nPages!=1 && <Button name="prev" size="sm" disabled={this.state.currentPage==0 ? true : false} onClick={this.onClick}>Previous</Button>}
+                    { this.props.nPages!=1 && this.state.currentPage+1} 
+                    { this.props.nPages!=1 && "/"}  
+                    { this.props.nPages!==1 && this.props.nPages}
+                    { this.props.nPages!=1 && <Button name="next" size="sm" disabled={this.state.currentPage==this.props.nPages-1 ? true : false} onClick={this.onClick}>Next</Button>}<br/>
                     selected course: {this.props.sCourse}
                 </Container>
                 </>;
@@ -44,22 +60,31 @@ class CoursePanel extends React.Component {
 };
 
 function CoursePanelRow(props) {
-    return <tr>
-        <td>{props.course.courseId}</td>
-        <td>{props.course.description}</td>
-        <td><Checkbox name={props.course.courseId} checked={props.checkedOne==props.course.courseId ? true : false} onChange={props.handler}/></td>
-    </tr>
+    if(props.nPages==1 || (props.nPages!=1 && props.pMap.get(props.course.courseId)==props.current))
+        return <tr>
+            <td>{props.course.courseId}</td>
+            <td>{props.course.description}</td>
+            <td><Checkbox name={props.course.courseId} checked={props.checkedOne==props.course.courseId ? true : false} onChange={props.handler}/></td>
+        </tr>
+    return <></>;
 }
 
 class LecturePanel extends React.Component {
 
     constructor(props){
         super(props);
-        this.state = {};
+        this.state = { currentPage: 0};
     }
 
     handler = (e) =>{
         this.props.update(e.target.name);
+    }
+
+    onClick = (e) =>{
+        if(e.target.name=="prev")
+            this.setState({currentPage: this.state.currentPage-1});
+        else //next
+            this.setState({currentPage: this.state.currentPage+1});
     }
 
     render(){
@@ -76,10 +101,15 @@ class LecturePanel extends React.Component {
                     </thead>
                     <tbody>
                     {
-                        this.props.lectures.map((lecture) => (<LecturePanelRow key={lecture.lectureId} checkedOne={this.props.sLecture} lecture={lecture} handler={this.handler}/>))
+                        this.props.lectures.map((lecture) => (<LecturePanelRow key={lecture.lectureId} checkedOne={this.props.sLecture} lecture={lecture} handler={this.handler} pMap={this.props.pageMap} nPages={this.props.nPages} current={this.state.currentPage}/>))
                     }
                     </tbody>
                 </Table>
+                    { this.props.nPages!=1 && <Button name="prev" size="sm" disabled={this.state.currentPage==0 ? true : false} onClick={this.onClick}>Previous</Button>}
+                    { this.props.nPages!=1 && this.state.currentPage+1} 
+                    { this.props.nPages!=1 && "/"}  
+                    {this.props.nPages!==1 && this.props.nPages}
+                    { this.props.nPages!=1 && <Button name="next" size="sm" disabled={this.state.currentPage==this.props.nPages-1 ? true : false} onClick={this.onClick}>Next</Button>}<br/>
                 selected lecture: {this.props.sLecture}
             </Container>
         </>;
@@ -87,17 +117,27 @@ class LecturePanel extends React.Component {
 }
 
 function LecturePanelRow(props) {
-    return <tr>
-        <td>{props.lecture.lectureId}</td>
-        <td>{props.lecture.date}</td>
-        <td><Checkbox name={props.lecture.lectureId} checked={props.checkedOne==props.lecture.lectureId ? true : false} onChange={props.handler}/></td>
-    </tr>
+    if(props.nPages==1 || (props.nPages!=1 && props.pMap.get(props.lecture.lectureId)==props.current))
+        return <tr>
+            <td>{props.lecture.lectureId}</td>
+            <td>{props.lecture.date}</td>
+            <td><Checkbox name={props.lecture.lectureId} checked={props.checkedOne==props.lecture.lectureId ? true : false} onChange={props.handler}/></td>
+        </tr>
+    return <></>;
 }
 
 class StudentPanel extends React.Component {
 
     constructor(props){
         super(props);
+        this.state={ currentPage: 0};
+    }
+
+    onClick = (e) =>{
+        if(e.target.name=="prev")
+            this.setState({currentPage: this.state.currentPage-1});
+        else //next
+            this.setState({currentPage: this.state.currentPage+1});
     }
 
     render(){
@@ -114,21 +154,28 @@ class StudentPanel extends React.Component {
                         </thead>
                         <tbody>
                         {
-                            this.props.students.map((student) => (<StudentPanelRow key={student.userId} student={student}/>))
+                            this.props.students.map((student) => (<StudentPanelRow key={student.userId} student={student} pMap={this.props.pageMap} nPages={this.props.nPages} current={this.state.currentPage}/>))
                         }
                         </tbody>
                     </Table>
+                    { this.props.nPages!=1 && <Button name="prev" size="sm" disabled={this.state.currentPage==0 ? true : false} onClick={this.onClick}>Previous</Button>}
+                    { this.props.nPages!=1 && this.state.currentPage+1} 
+                    { this.props.nPages!=1 && "/"}  
+                    {this.props.nPages!==1 && this.props.nPages}
+                    { this.props.nPages!=1 && <Button name="next" size="sm" disabled={this.state.currentPage==this.props.nPages-1 ? true : false} onClick={this.onClick}>Next</Button>}<br/>
                 </Container>
                 </>;
     }
 };
 
 function StudentPanelRow(props) {
-    return <tr>
-        <td>{props.student.lastName}</td>
-        <td>{props.student.firstName}</td>
-        <td>{props.student.userId}</td>
-    </tr>
+    if(props.nPages==1 || (props.nPages!=1 && props.pMap.get(props.student.userId)==props.current))
+        return <tr>
+            <td>{props.student.lastName}</td>
+            <td>{props.student.firstName}</td>
+            <td>{props.student.userId}</td>
+        </tr>
+    return <></>;
 }
 
 export {CoursePanel,LecturePanel,StudentPanel};
