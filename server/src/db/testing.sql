c4860f3d1633661b4ed62d4118ae48c976a59942
paologastaldi-polito <s277393@studenti.polito.it>
2020-11-09 11:32:52 +0100

    feat: test query to init the DB

---
+++ server/src/db/testing.sql
@@ -0,0 +1,10 @@
+INSERT INTO User(type, firstName, lastName, email, password) VALUES('STUDENT', 'Aldo', 'Baglio', 'aldo.baglio@agg.it', 'aldo');
+INSERT INTO User(type, firstName, lastName, email, password) VALUES('STUDENT', 'Giovanni', 'Storti', 'giovanni.storti@agg.it', 'giovanni');
+INSERT INTO User(type, firstName, lastName, email, password) VALUES('STUDENT', 'Silvana', 'Fallisi', 'silvana.fallisi@agg.it', 'silvana');
+
+INSERT INTO Course(description, year) VALUES('Software enginnering 2', 2020);
+INSERT INTO Course(description, year) VALUES('Computer system security', 2020);
+INSERT INTO Course(description, year) VALUES('Machine learning and artificial intelligence', 2020);
+
+INSERT INTO User(type, firstName, lastName, email, password) VALUES('TEACHER', 'Giacomo', 'Poretti', 'giacomo.poretti@agg.it', 'giacomo');
+INSERT INTO User(type, firstName, lastName, email, password) VALUES('TEACHER', 'Marina', 'Massironi', 'marina.massironi@agg.it', 'marina');
