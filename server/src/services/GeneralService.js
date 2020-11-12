3bfd10c6d07517d32c3120d81b4548ae5b9ca99c
paologastaldi-polito <s277393@studenti.polito.it>
2020-11-12 18:16:34 +0100

    feat: general management

--- server/src/services/GeneralService.js
+++ server/src/services/GeneralService.js
@@ -1,27 +1,22 @@
+/**
+ * General dao accesses
+ * @author Gastaldi Paolo
+ * @version 1.0.0
+ */
 "use strict";
 
+const dao = require('../db/Dao.js');
+const User = require('../entities/User.js');
+
 /**
- * User request to sign in system
- *
- * body Body { email: 'fake@email.com', password: 'password' } 
- * returns a Student
- **/
-exports.userLogin = function (body) {
-  console.log(body);
-  return new Promise(function (resolve, reject) {
-    let examples = {};
-    examples["application/json"] = {
-      firstName: "foo",
-      lastName: "bar",
-      password: "password",
-      type: "Student",
-      userId: 1,
-      email: "fake@email.com",
-    };
-    if (Object.keys(examples).length > 0) {
-      resolve(examples[Object.keys(examples)[0]]);
-    } else {
-      resolve();
-    }
-  });
+ * perform login
+ * @param {Object} body - email and password needed
+ * @returns {Promise<Teacher|Student>} promise
+ */
+exports.userLogin = function(body) {
+    const user = new User();
+    user.email = body.email;
+    user.password = body.password;
+
+    return dao.login(user);
 };
