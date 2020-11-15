2df29c723aa20528e1ff65713bcab4d17dc8bf61
Francesco Xia <s276509@studenti.polito.it>
2020-11-15 14:31:46 +0100

    fix: PULSBS.db image

--- server/src/server.js
+++ server/src/server.js
@@ -35,7 +35,7 @@ EmailService.sendConfirmationBookingEmail("fakeStudent.se2866755766@gmail.com", 
 */
 
 morgan.token("host", function (req) {
-  return "src: " + req.hostname;
+    return "src: " + req.hostname;
 });
 app.use(morgan(":method :url :host code: :status :res[content-length] - :response-time ms"));
 
@@ -46,10 +46,10 @@ app.use(`${BASE_ROUTE}`, General);
 
 // The following routes needs authentication
 app.use((err, req, res, next) => {
-  if (!req.cookies) res.status(401).json("login must be performed before this action");
-  jwt.verify(req.cookies.token, { key: JWT_SECRET }, (err, decoded) => {
-    if (err) res.status(401).json("invalid login token");
-  });
+    if (!req.cookies) res.status(401).json("login must be performed before this action");
+    jwt.verify(req.cookies.token, { key: JWT_SECRET }, (error, decoded) => {
+        if (error) res.status(401).json("invalid login token");
+    });
 });
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -61,8 +61,8 @@ app.use(`${BASE_ROUTE}/students`, Student);
 
 // TEACHER ROUTES
 app.get(
-  `${BASE_ROUTE}/teachers/:teacherId/courses/:courseId/lectures/:lectureId/students`,
-  Teacher.teacherGetCourseLectureStudents
+    `${BASE_ROUTE}/teachers/:teacherId/courses/:courseId/lectures/:lectureId/students`,
+    Teacher.teacherGetCourseLectureStudents
 );
 
 app.get(`${BASE_ROUTE}/teachers/:teacherId/courses/:courseId/lectures`, Teacher.teacherGetCourseLectures);
@@ -75,32 +75,37 @@ app.all(`${BASE_ROUTE}`, () => console.log("This route is not supported. Check t
 ////////////////////////////////////////////////////////////////////////////////
 
 app.get(`${BASE_ROUTE}/reset`, async (req, res) => {
-  try {
-    const path = "./testing.db";
-    await prepareDb(path);
-    res.status(200).send("Success Reset");
-  } catch (err) {
-    res.status(400).send("Failed Reset");
-  }
+    try {
+        // TODO: add a prompt in case db == PULSB.db
+        const path = dbPath;
+        await prepareDb(dbPath);
+
+        res.status(200).send("Success Reset");
+    } catch (err) {
+        res.status(400).send("Failed Reset");
+    }
 });
 
 function printConfig() {
-  console.log(`Server running on http://localhost:${PORT}${BASE_ROUTE}\n`);
-  console.log("System parameters:");
-  console.log(`DB path: ${dbPath}`);
+    console.log(`Server running on http://localhost:${PORT}${BASE_ROUTE}\n`);
+    console.log("System parameters:");
+    console.log(`DB path: ${dbPath}`);
 }
 
 // "Main"
 (async () => {
-  try {
-    console.log("INITIALIZING the system");
-    if (process.argv[2] === "--test") {
-      dbPath = "./testing.db";
+    try {
+        console.log("INITIALIZING the system");
+
+        if (process.argv[2] === "--test") {
+            dbPath = "./testing.db";
+        }
+
+        await db.init(dbPath);
+
+        app.listen(PORT, printConfig);
+    } catch (err) {
+        console.log(err, "FAILED initializing the system");
+        return process.exit(-1);
     }
-    await db.init(dbPath);
-    app.listen(PORT, printConfig);
-  } catch (err) {
-    console.log(err, "FAILED initializing the system");
-    return process.exit(-1);
-  }
 })();
