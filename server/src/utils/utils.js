3d6c27f818393826a1a1941f2fe005c4a83059b4
paologastaldi-polito <s277393@studenti.polito.it>
2020-11-18 16:31:04 +0100

    fix: input validation errors

---
+++ server/src/utils/utils.js
@@ -0,0 +1,21 @@
+/**
+ * add here general functions
+ * @author Gastaldi Paolo
+ * @version 1.0.0
+ */
+
+ /**
+  * adapter for express-validator errors
+  * to create a string error message
+  * @param {Array} errors
+  * @return {String} error message
+  */
+const errToString = function(errors) {
+    if(!(errors && errors.errors && Array.isArray(errors.errors) && errors.errors.length > 0))
+        return '';
+
+    const err = errors.errors[0];
+    const msg = err.msg + ' ' + err.param;
+    return msg;
+}
+exports.errToString = errToString;
