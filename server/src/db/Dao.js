c444aa6748c0a80cbf2c8c6f2c8fb963c52fbf9e
Francesco Xia <s276509@studenti.polito.it>
2020-11-24 18:47:54 +0100

    feat: add get, delete, and update lecture

--- server/src/db/Dao.js
+++ server/src/db/Dao.js
@@ -564,3 +564,72 @@ const getLecturesByPeriodOfTime = function(course, periodOfTime) {
     });
 };
 exports.getLecturesByPeriodOfTime = getLecturesByPeriodOfTime;
+
+/**
+ * get a lecture given a lectureId 
+ * @param {Lecture} lecture - lectureId needed
+ * @returns {Promise} promise
+ */
+const getLectureById = function(lecture) {
+    return new Promise((resolve, reject) => {
+        const sql = `SELECT * FROM Lecture
+            WHERE Lecture.lectureId = ?`;
+
+        db.get(sql, [lecture.lectureId], (err, row) => {
+            if(err) {
+                reject(StandardErr.fromDao(err));
+                return;
+            }
+            
+            if(!row) {
+                resolve(null);
+                return;
+            }
+
+            resolve(Lecture.from(row));
+        });
+    })
+}
+exports.getLectureById = getLectureById;
+
+/**
+ * delete a lecture given a lectureId 
+ * @param {Lecture} lecture - lectureId needed
+ * @returns {Promise} promise
+ */
+const deleteLecture = function(lecture) {
+    return new Promise((resolve, reject) => {
+        const sql = `DELETE * FROM Lecture WHERE lectureId = ?`;
+
+        db.run(sql, [lecture.lectureId], function(err) {
+            if(err) {
+                reject(StandardErr.fromDao(err));
+                return;
+            }
+
+            resolve(this.changes);
+        });
+    });
+}
+exports.deleteLecture = deleteLecture;
+
+/**
+ * update a lecture delivery mode 
+ * @param {Lecture} lecture - delivery mode needed
+ * @returns {Promise} promise
+ */
+const updateLectureDeliveryMode = function(lecture) {
+    return new Promise((resolve, reject) => {
+        const sql = `UPDATE Lecture SET delivery = ? WHERE lectureId = ?`;
+
+        db.run(sql, [lecture.delivery, lecture.lectureId], function(err) {
+            if(err) {
+                reject(StandardErr.fromDao(err));
+                return;
+            }
+
+            resolve(this.changes);
+        });
+    });
+}
+exports.updateLectureDeliveryMode = updateLectureDeliveryMode;
