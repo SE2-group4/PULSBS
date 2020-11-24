be0a157f03e2b3808eb8460a948a608f5cb32303
Alberto <pisanellof@hotmail.it>
2020-11-24 18:47:51 +0100

    [PULSBS-66] added delete logic

--- client/src/pages/TeacherPage.js
+++ client/src/pages/TeacherPage.js
@@ -3,14 +3,14 @@ import Container from "react-bootstrap/Container"
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import API from "../api/Api";
-import {CoursePanel,LecturePanel,StudentPanel,EditModal} from '../components/TeacherComp';
+import { CoursePanel, LecturePanel, StudentPanel, EditModal, DeleteModal } from '../components/TeacherComp';
 import Lecture from '../entities/lecture';
 
 /**
  *  elementForPage is a configuration parameter to limit the number of entries of the tables showing courses/lectures/students
  *  in the same instance (min value: 2)
  */
-const elementForPage=2;
+const elementForPage = 2;
 
 class TeacherPage extends React.Component {
 
@@ -18,160 +18,210 @@ class TeacherPage extends React.Component {
      * TeacherPage constructor
      * @param {User} props 
      */
-    constructor(props){
+    constructor(props) {
         super(props);
-        this.state = {user : props.user,
-                        courses : [],lectures : [],students : [],                       //elements
-                        selectedCourse: null,selectedLecture: null,                     //selected elements
-                        lectureIdToUpdate: null,deliveryToUpdate: null,                 //change modality management
-                        courseMap: new Map(),cPages: 1,                                 //course pagination
-                        lectureMap: new Map(),lPages: 1,                                //lecture pagination
-                        studentMap: new Map(),sPages: 1,                                //student pagination
-                        fetchErrorC: false,fetchErrorL: false,fetchErrorS: false };     //fetch errors
+        this.state = {
+            user: props.user,
+            courses: [], lectures: [], students: [],                       //elements
+            selectedCourse: null, selectedLecture: null,                     //selected elements
+            lectureIdToUpdate: null, deliveryToUpdate: null,                 //change modality management
+            lectureIdToDelete: null,                                        //delete lecture
+            courseMap: new Map(), cPages: 1,                                 //course pagination
+            lectureMap: new Map(), lPages: 1,                                //lecture pagination
+            studentMap: new Map(), sPages: 1,                                //student pagination
+            fetchErrorC: false, fetchErrorL: false, fetchErrorS: false
+        };     //fetch errors
     }
-    
+
     /**
      * componentDidMount fetches the all courses of the teacher 
      */
-    componentDidMount(){
+    componentDidMount() {
         API.getCoursesByTeacherId(this.state.user.userId)
-        .then((courses)=>{
-            let i=0;
-            let nMap=new Map(); 
-            //courses.map((c)=>{
-            //nMap.set(c.courseId,Math.floor(i/elementForPage));
-            //i++;})
-            courses.forEach(function(item){
-                nMap.set(item.courseId,Math.floor(i/elementForPage));
-                i++;
-            });
-            let nPages=Math.ceil(i/elementForPage);
-            //console.log(courses);
-            this.setState({courses : courses,courseMap: nMap,cPages: nPages,fetchErrorC: false});
-        })
-        .catch(() =>{ this.setState({fetchErrorC: true}); });
+            .then((courses) => {
+                let i = 0;
+                let nMap = new Map();
+                //courses.map((c)=>{
+                //nMap.set(c.courseId,Math.floor(i/elementForPage));
+                //i++;})
+                courses.forEach(function (item) {
+                    nMap.set(item.courseId, Math.floor(i / elementForPage));
+                    i++;
+                });
+                let nPages = Math.ceil(i / elementForPage);
+                //console.log(courses);
+                this.setState({ courses: courses, courseMap: nMap, cPages: nPages, fetchErrorC: false });
+            })
+            .catch(() => { this.setState({ fetchErrorC: true }); });
     }
 
     /**
      * updateLectures fetches all lectures of the selected teacher's course 
      */
-    updateLectures = (courseId) =>{
-        API.getLecturesByCourseIdByTeacherId(this.state.user.userId,courseId)
-        .then((lectures)=>{
-            let i=0;
-            let nMap=new Map(); 
-            //lectures.map((l)=>{
-            //nMap.set(l.lectureId,Math.floor(i/elementForPage));
-            //i++;})
-            lectures.forEach(function(item){
-                nMap.set(item.lectureId,Math.floor(i/elementForPage));
-                i++;
-            });
-            let nPages=Math.ceil(i/elementForPage);
-            //console.log(lectures);
-            this.setState({lectures : lectures,lectureMap: nMap,lPages: nPages,selectedCourse: courseId,selectedLecture: null,fetchErrorL: false,students: [],sPages: 1,fetchErrorS: false});
-        })
-        .catch(() =>{ this.setState({selectedCourse: courseId,lectures: [],selectedLecture: null,lPages: 1,fetchErrorL: true,students: [],sPages: 1,fetchErrorS: false}); });
+    updateLectures = (courseId) => {
+        API.getLecturesByCourseIdByTeacherId(this.state.user.userId, courseId)
+            .then((lectures) => {
+                let i = 0;
+                let nMap = new Map();
+                //lectures.map((l)=>{
+                //nMap.set(l.lectureId,Math.floor(i/elementForPage));
+                //i++;})
+                lectures.forEach(function (item) {
+                    nMap.set(item.lectureId, Math.floor(i / elementForPage));
+                    i++;
+                });
+                let nPages = Math.ceil(i / elementForPage);
+                //console.log(lectures);
+                this.setState({ lectures: lectures, lectureMap: nMap, lPages: nPages, selectedCourse: courseId, selectedLecture: null, fetchErrorL: false, students: [], sPages: 1, fetchErrorS: false });
+            })
+            .catch(() => { this.setState({ selectedCourse: courseId, lectures: [], selectedLecture: null, lPages: 1, fetchErrorL: true, students: [], sPages: 1, fetchErrorS: false }); });
     }
 
     /**
      * updateStudents fetches all students of the selected lecture of a teacher's selected course 
      */
-    updateStudents = (lectureId) =>{
-        API.getStudentsByLecture(this.state.user.userId,this.state.selectedCourse,lectureId)
-        .then((students)=>{
-            let i=0;
-            let nMap=new Map(); 
-            //students.map((s)=>{
-            //nMap.set(s.studentId,Math.floor(i/elementForPage));
-            //i++;})
-            students.forEach(function(item){
-                nMap.set(item.studentId,Math.floor(i/elementForPage));
-                i++;
-            });
-            let nPages=Math.ceil(i/elementForPage);
-            this.setState({students : students,studentMap: nMap,sPages: nPages,selectedLecture: lectureId,fetchErrorS: false})
-        })
-        .catch(() =>{ this.setState({selectedLecture: lectureId,students: [],sPages: 1,fetchErrorS: true}); });
+    updateStudents = (lectureId) => {
+        API.getStudentsByLecture(this.state.user.userId, this.state.selectedCourse, lectureId)
+            .then((students) => {
+                let i = 0;
+                let nMap = new Map();
+                //students.map((s)=>{
+                //nMap.set(s.studentId,Math.floor(i/elementForPage));
+                //i++;})
+                students.forEach(function (item) {
+                    nMap.set(item.studentId, Math.floor(i / elementForPage));
+                    i++;
+                });
+                let nPages = Math.ceil(i / elementForPage);
+                this.setState({ students: students, studentMap: nMap, sPages: nPages, selectedLecture: lectureId, fetchErrorS: false })
+            })
+            .catch(() => { this.setState({ selectedLecture: lectureId, students: [], sPages: 1, fetchErrorS: true }); });
     }
 
     /**
      * resetSelected lets to reset the selected course/lecture (and related states)
      */
-    resetSelected = (type) =>{
-        switch(type){
+    resetSelected = (type) => {
+        switch (type) {
             case "course":
-                this.setState({selectedCourse: null,lectures: [],selectedLecture: null,lPages: 1,fetchErrorL: false,students: [],sPages: 1,fetchErrorS: false})
-            break;
+                this.setState({ selectedCourse: null, lectures: [], selectedLecture: null, lPages: 1, fetchErrorL: false, students: [], sPages: 1, fetchErrorS: false })
+                break;
             case "lecture":
-                this.setState({selectedLecture: null,students: [],sPages: 1,fetchErrorS: false});
-            break;
+                this.setState({ selectedLecture: null, students: [], sPages: 1, fetchErrorS: false });
+                break;
             default:
-            break;
+                break;
         }
     }
 
     // EditModal handlers
-    showEditModal = (lectureId,delivery) =>{
-        this.setState({lectureIdToUpdate: lectureId,deliveryToUpdate: delivery});
+    showEditModal = (lectureId, delivery) => {
+        this.setState({ lectureIdToUpdate: lectureId, deliveryToUpdate: delivery });
     }
 
-    closeEditModal = () =>{
-        this.setState({lectureIdToUpdate: null,deliveryToUpdate: null});
+    // EditModal handlers
+    showDeleteModal = (lectureId) => {
+        this.setState({ lectureIdToDelete: lectureId });
     }
 
-    updateDelivery = () =>{
-        var deliveryToUpdate=this.state.deliveryToUpdate;
-        var newDel=deliveryToUpdate==1 ? 2 : 1;
+    closeEditModal = () => {
+        this.setState({ lectureIdToUpdate: null, deliveryToUpdate: null });
+    }
+
+    closeDeleteModal = () => {
+        this.setState({ lectureIdToDelete: null });
+    }
+
+    updateDelivery = () => {
+        var deliveryToUpdate = this.state.deliveryToUpdate;
+        var newDel = deliveryToUpdate == 1 ? 2 : 1;
         //API.updateDeliveryByLecture(this.state.user.userId,this.state.selectedCourse,this.state.lectureIdToUpdate,newDel)
         //.then(())=>{
         //    
         //}).catch(()=>{});
-        var newLectures=this.state.lectures.slice();
-        var newLecture,i;
-        var lectureIdToUpdate=this.state.lectureIdToUpdate;
+        var newLectures = this.state.lectures.slice();
+        var newLecture, i;
+        var lectureIdToUpdate = this.state.lectureIdToUpdate;
         //console.log(lectureIdToUpdate);
-        newLectures.forEach(function (item,index){
+        newLectures.forEach(function (item, index) {
             //console.log(item.lectureId+" "+lectureIdToUpdate);
-            if(item.lectureId==lectureIdToUpdate){
-                newLecture=new Lecture(item.lectureId,item.courseId,item.classId,item.startingDate,item.duration,item.bookingDeadline,newDel);
-                i=index;
+            if (item.lectureId == lectureIdToUpdate) {
+                newLecture = new Lecture(item.lectureId, item.courseId, item.classId, item.startingDate, item.duration, item.bookingDeadline, newDel);
+                i = index;
             }
         });
         //console.log(newLecture);
         //console.log(i);
-        newLectures.splice(i,1,newLecture);
-        this.setState({lectures: newLectures,lectureIdToUpdate: null,deliveryToUpdate: null});
+        newLectures.splice(i, 1, newLecture);
+        this.setState({ lectures: newLectures, lectureIdToUpdate: null, deliveryToUpdate: null });
     }
 
-    render(){
-        return(
+    deleteLecture = () => {
+        //TODO api call
+
+        var newLectures = this.state.lectures.slice();
+        var i;
+        var lectureIdToDelete = this.state.lectureIdToDelete;
+        //console.log(lectureIdToUpdate);
+        newLectures.forEach(function (item, index) {
+            //console.log(item.lectureId+" "+lectureIdToUpdate);
+            if (item.lectureId == lectureIdToDelete) {
+                i = index;
+            }
+        });
+        //console.log(newLecture);
+        //console.log(i);
+        newLectures.splice(i, 1);
+
+        i = 0;
+        let nMap = new Map();
+        //lectures.map((l)=>{
+        //nMap.set(l.lectureId,Math.floor(i/elementForPage));
+        //i++;})
+        newLectures.forEach(function (item) {
+            nMap.set(item.lectureId, Math.floor(i / elementForPage));
+            i++;
+        });
+        let nPages = Math.ceil(i / elementForPage);
+        if (lectureIdToDelete === this.state.selectedLecture) {
+            this.setState({ lectures: newLectures, lectureIdToDelete: null, lectureMap: nMap, lPages: nPages, selectedLecture: null, students: [], sPages: 1 });
+        }
+        else {
+            this.setState({ lectures: newLectures, lectureIdToDelete: null, lectureMap: nMap, lPages: nPages });
+        }
+    }
+
+    render() {
+        return (
             <>
-             { this.state.lectureIdToUpdate &&
-            <EditModal editClose={this.closeEditModal} lectureId={this.state.lectureIdToUpdate} delivery={this.state.deliveryToUpdate} updateDelivery={this.updateDelivery}/>}
-            <Container fluid>
-                <Row>
-                <Col sm={8}>
-                <CoursePanel courses={this.state.courses} fetchError={this.state.fetchErrorC}                   //courses
-                sCourse={this.state.selectedCourse} pageMap={this.state.courseMap} nPages={this.state.cPages}   //courses pagination
-                update={this.updateLectures} reset={this.resetSelected}                                         //interaction with Lecture Panel
-                />
-                <br/>
-                <br/>
-                <LecturePanel lectures={this.state.lectures} fetchError={this.state.fetchErrorL}                                            //lectures
-                sLecture={this.state.selectedLecture} pageMap={this.state.lectureMap} nPages={this.state.lPages}                            //lectures pagination
-                update={this.updateStudents} reset={this.resetSelected}                                                                     //interaction with Student Panel                                                 
-                lectureId={this.state.lectureIdToUpdate} deliveryToUpdate={this.state.deliveryToUpdate} showEditModal={this.showEditModal}  //EditModality management                                                                     
-                />   
-                <br/>
-                <br/>
-                <StudentPanel students={this.state.students} fetchError={this.state.fetchErrorS}    //students
-                pageMap={this.state.studentMap} nPages={this.state.sPages}                          //students pagination
-                />
-                </Col>
-                <Col sm={4}><> </></Col>
-                </Row>
-            </Container>
+                { this.state.lectureIdToUpdate &&
+                    <EditModal editClose={this.closeEditModal} lectureId={this.state.lectureIdToUpdate} delivery={this.state.deliveryToUpdate} updateDelivery={this.updateDelivery} />}
+                { this.state.lectureIdToDelete &&
+                    <DeleteModal deleteClose={this.closeDeleteModal} lectureId={this.state.lectureIdToDelete} deleteLecture={this.deleteLecture} />}
+                <Container fluid>
+                    <Row>
+                        <Col sm={8}>
+                            <CoursePanel courses={this.state.courses} fetchError={this.state.fetchErrorC}                   //courses
+                                sCourse={this.state.selectedCourse} pageMap={this.state.courseMap} nPages={this.state.cPages}   //courses pagination
+                                update={this.updateLectures} reset={this.resetSelected}                                         //interaction with Lecture Panel
+                            />
+                            <br />
+                            <br />
+                            <LecturePanel lectures={this.state.lectures} fetchError={this.state.fetchErrorL}                                            //lectures
+                                sLecture={this.state.selectedLecture} pageMap={this.state.lectureMap} nPages={this.state.lPages}                            //lectures pagination
+                                update={this.updateStudents} reset={this.resetSelected}                                                                     //interaction with Student Panel                                                 
+                                showEditModal={this.showEditModal}  //EditModality management                                                                     
+                                showDeleteModal={this.showDeleteModal} //Delete
+                            />
+                            <br />
+                            <br />
+                            <StudentPanel students={this.state.students} fetchError={this.state.fetchErrorS}    //students
+                                pageMap={this.state.studentMap} nPages={this.state.sPages}                          //students pagination
+                            />
+                        </Col>
+                        <Col sm={4}><> </></Col>
+                    </Row>
+                </Container>
             </>
         );
     }
