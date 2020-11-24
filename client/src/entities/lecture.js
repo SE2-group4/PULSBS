828e2a77105fd47522434fbe4dba8822cf312099
Neppa97 <lorenzoappendini@gmail.com>
2020-11-24 12:37:57 +0100

    fixed teacher date handling

--- client/src/entities/lecture.js
+++ client/src/entities/lecture.js
@@ -1,9 +1,10 @@
 class Lecture{
-    constructor(lectureId,courseId,classId,startingDate,bookingDeadline,delivery){
+    constructor(lectureId,courseId,classId,startingDate,duration,bookingDeadline,delivery){
         this.lectureId=lectureId;
         this.courseId=courseId;
         this.classId=classId;
         this.startingDate=startingDate;
+        this.duration=duration;
         this.bookingDeadline=bookingDeadline;
         this.delivery=delivery;
     }
