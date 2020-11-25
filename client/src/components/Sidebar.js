1b0455afe159939036885c724e64bcda1cdd6207
LorenzoCeccarelli <56482293+LorenzoCeccarelli@users.noreply.github.com>
2020-11-25 15:45:01 +0100

    [PULSBS-60] finish calendar component

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
-import ListGroup from 'react-bootstrap/ListGroup';
 import Badge from 'react-bootstrap/Badge';
 
-function Sidebar(props){
-    if (!props.courses)
-      return(<></>)
+function Sidebar(){
   return (
     <Container fluid id="sidebarCalendar">
       <strong>Legend:</strong>
@@ -16,6 +13,8 @@ function Sidebar(props){
       <Badge variant="dark">Over</Badge>
       <br></br>
       <Badge variant="danger">Expired</Badge>
+      <br></br>
+      <Badge variant="secondary">Remote</Badge>
       <br></br>
     </Container>
   )
