fd3ae4a2b38dcc0882791d8a89483fedb4409946
paologastaldi-polito <s277393@studenti.polito.it>
2020-11-25 13:01:04 +0100

    refactor: test reorganization

--- server/tests/Test_email.js
+++ server/tests/Test_email.js
@@ -9,219 +9,221 @@ const nodemailerMock = require('nodemailer-mock');
 var EmailService;
 
 const suite = function() {
-    describe('Tests EmailService.sendConfirmationBookingEmail', async () => {
-        let app = null;
+    describe('EmailService', function() {
+        describe('Tests EmailService.sendConfirmationBookingEmail', async () => {
+            let app = null;
 
-        before(async () => {
-            // Enable mockery to mock objects
-            mockery.enable({
-                warnOnUnregistered: false,
-                useCleanCache: true,
+            before(async () => {
+                // Enable mockery to mock objects
+                mockery.enable({
+                    warnOnUnregistered: false,
+                    useCleanCache: true,
+                });
+                mockery.registerMock('nodemailer', nodemailerMock);
+                EmailService = require('../src/services/EmailService');
+
             });
-            mockery.registerMock('nodemailer', nodemailerMock);
-            EmailService = require('../src/services/EmailService');
 
-        });
-
-        afterEach(async () => {
-            nodemailerMock.mock.reset();
-        });
-
-        after(async () => {
-            // Remove our mocked nodemailer and disable mockery
-            mockery.deregisterAll();
-            mockery.disable();
-        });
-
-        it('should send a confirmation booking email using nodemailer-mock', async () => {
-
-            const response = EmailService.sendConfirmationBookingEmail("s3945734658376e@gmail.com", "SE2", "13:00"); // <-- your email code here
-            response.then((done) => {
-                done.response.should.be.exactly("nodemailer-mock success");
-            }).catch((err) => {
-                console.log(err);
-            })
-        });
-
-        it('should fail to send a confirmation booking email using nodemailer-mock', async () => {
-            // tell the mock class to return an error
-            const err = new Error('My custom error');
-            nodemailerMock.mock.setShouldFailOnce();
-            nodemailerMock.mock.setFailResponse(err);
-
-            // call a service that uses nodemailer
-            const response = EmailService.sendConfirmationBookingEmail("fakeStudent.se2@gmail.com", "SE2", "13:00"); // <-- your code here
-
-            // a test for our response
-            response.then((done) => {
-                response.error.should.be.exactly(err);
-            })
-        });
-
-        it('should verify using the real nodemailer transport of sendConfirmationBookingEmail', async () => {
-            // tell the mock class to pass verify requests to nodemailer
-            nodemailerMock.mock.setMockedVerify(false);
-
-            // call a service that uses nodemailer
-            const response = EmailService.sendConfirmationBookingEmail("fakeStudent.se2@gmail.com", "SE2", "13:00"); // <-- your code here
-
-            response.then((done) => {
-                done.response.should.be.exactly("nodemailer-mock success");
-            }).catch((err) => {
-                console.log(err);
-            })
-        });
-        it ("should verify that the confirmation booking email cannot send because the to is a nullish value", async()=>{
-            const response = EmailService.sendConfirmationBookingEmail(null, "SE2", "13:00");
-            response.catch((err)=>{
-                console.log(err);
-                err.should.be.exactly("Undefined recipient")
-            })
-        });
-    })
-    describe('Tests EmailService.sendStudentNumberEmail', async () => {
-        let app = null;
-
-        before(async () => {
-            // Enable mockery to mock objects
-            mockery.enable({
-                warnOnUnregistered: false,
-                useCleanCache: true,
+            afterEach(async () => {
+                nodemailerMock.mock.reset();
             });
-            mockery.registerMock('nodemailer', nodemailerMock);
-            EmailService = require('../src/services/EmailService');
 
-        });
-
-        afterEach(async () => {
-            nodemailerMock.mock.reset();
-        });
-
-        after(async () => {
-            // Remove our mocked nodemailer and disable mockery
-            mockery.deregisterAll();
-            mockery.disable();
-        });
-
-        it('should send a number of student  email using nodemailer-mock', async () => {
-
-            const response = EmailService.sendStudentNumberEmail("s3945734658376e@gmail.com", "SE2", "13:00",3); // <-- your email code here
-            response.then((done) => {
-                //console.log(done);
-                done.response.should.be.exactly("nodemailer-mock success");
-            }).catch((err) => {
-                console.log(err);
-            })
-        });
-
-        it('should fail to send an email using nodemailer-mock', async () => {
-            // tell the mock class to return an error
-            const err = new Error('My custom error');
-            nodemailerMock.mock.setShouldFailOnce();
-            nodemailerMock.mock.setFailResponse(err);
-
-            // call a service that uses nodemailer
-            const response = EmailService.sendStudentNumberEmail("fakeStudent.se2@gmail.com", "SE2", "13:00",3); // <-- your code here
-
-            // a test for our response
-            response.then((done) => {
-                response.error.should.be.exactly(err);
-            })
-        });
-
-        it('should verify using the real nodemailer transport', async () => {
-            // tell the mock class to pass verify requests to nodemailer
-            nodemailerMock.mock.setMockedVerify(false);
-
-            // call a service that uses nodemailer
-            const response = EmailService.sendStudentNumberEmail("fakeStudent.se2@gmail.com", "SE2", "13:00",3); // <-- your code here
-
-            /* calls to transport.verify() will be passed through, 
-            transport.sendMail() is still mocked */
-            response.then((done) => {
-                done.response.should.be.exactly("nodemailer-mock success");
-            }).catch((err) => {
-                console.log(err);
-            })
-        });
-        it ("should verify that the email cannot send because the to is a nullish value", async()=>{
-            const response = EmailService.sendStudentNumberEmail(null, "SE2", "13:00",3); // <-- your code here
-            response.catch((err)=>{
-                console.log(err);
-                err.should.be.exactly("Undefined recipient")
-            })
-        });
-    })
-    describe('Tests EmailService.sendCustomEmail', async () => {
-        let app = null;
-
-        before(async () => {
-            // Enable mockery to mock objects
-            mockery.enable({
-                warnOnUnregistered: false,
-                useCleanCache: true,
+            after(async () => {
+                // Remove our mocked nodemailer and disable mockery
+                mockery.deregisterAll();
+                mockery.disable();
             });
-            mockery.registerMock('nodemailer', nodemailerMock);
-            EmailService = require('../src/services/EmailService');
 
-        });
+            it('should send a confirmation booking email using nodemailer-mock', async () => {
 
-        afterEach(async () => {
-            nodemailerMock.mock.reset();
-        });
+                const response = EmailService.sendConfirmationBookingEmail("s3945734658376e@gmail.com", "SE2", "13:00"); // <-- your email code here
+                response.then((done) => {
+                    done.response.should.be.exactly("nodemailer-mock success");
+                }).catch((err) => {
+                    console.log(err);
+                })
+            });
 
-        after(async () => {
-            // Remove our mocked nodemailer and disable mockery
-            mockery.deregisterAll();
-            mockery.disable();
-        });
+            it('should fail to send a confirmation booking email using nodemailer-mock', async () => {
+                // tell the mock class to return an error
+                const err = new Error('My custom error');
+                nodemailerMock.mock.setShouldFailOnce();
+                nodemailerMock.mock.setFailResponse(err);
 
-        it('should send a custom e-mail using nodemailer-mock', async () => {
+                // call a service that uses nodemailer
+                const response = EmailService.sendConfirmationBookingEmail("fakeStudent.se2@gmail.com", "SE2", "13:00"); // <-- your code here
 
-            const response = EmailService.sendCustomMail("s3945734658376e@gmail.com", "Test", "This is a test"); // <-- your email code here
-            response.then((done) => {
-                //console.log(done);
-                done.response.should.be.exactly("nodemailer-mock success");
-            }).catch((err) => { 
-                console.log(err);
-            })
-        });
+                // a test for our response
+                response.then((done) => {
+                    response.error.should.be.exactly(err);
+                })
+            });
 
-        it('should fail to send a custom e-mail using nodemailer-mock', async () => {
-            // tell the mock class to return an error
-            const err = new Error('My custom error');
-            nodemailerMock.mock.setShouldFailOnce();
-            nodemailerMock.mock.setFailResponse(err);
+            it('should verify using the real nodemailer transport of sendConfirmationBookingEmail', async () => {
+                // tell the mock class to pass verify requests to nodemailer
+                nodemailerMock.mock.setMockedVerify(false);
 
-            // call a service that uses nodemailer
-            const response = EmailService.sendCustomMail("s3945734658376e@gmail.com", "Test", "This is a test"); // <-- your code here
+                // call a service that uses nodemailer
+                const response = EmailService.sendConfirmationBookingEmail("fakeStudent.se2@gmail.com", "SE2", "13:00"); // <-- your code here
 
-            // a test for our response
-            response.then((done) => {
-                response.error.should.be.exactly(err);
-            })
-        });
+                response.then((done) => {
+                    done.response.should.be.exactly("nodemailer-mock success");
+                }).catch((err) => {
+                    console.log(err);
+                })
+            });
+            it ("should verify that the confirmation booking email cannot send because the to is a nullish value", async()=>{
+                const response = EmailService.sendConfirmationBookingEmail(null, "SE2", "13:00");
+                response.catch((err)=>{
+                    console.log(err);
+                    err.should.be.exactly("Undefined recipient")
+                })
+            });
+        })
+        describe('Tests EmailService.sendStudentNumberEmail', async () => {
+            let app = null;
 
-        it('should verify using the real nodemailer transport', async () => {
-            // tell the mock class to pass verify requests to nodemailer
-            nodemailerMock.mock.setMockedVerify(false);
+            before(async () => {
+                // Enable mockery to mock objects
+                mockery.enable({
+                    warnOnUnregistered: false,
+                    useCleanCache: true,
+                });
+                mockery.registerMock('nodemailer', nodemailerMock);
+                EmailService = require('../src/services/EmailService');
 
-            // call a service that uses nodemailer
-            const response = EmailService.sendCustomMail("s3945734658376e@gmail.com", "Test", "This is a test"); // <-- your code here
+            });
 
-            /* calls to transport.verify() will be passed through, 
-            transport.sendMail() is still mocked */
-            response.then((done) => {
-                done.response.should.be.exactly("nodemailer-mock success");
-            }).catch((err) => {
-                console.log(err);
-            })
-        });
-        it ("should verify that the custom email cannot send because the to is a nullish value", async()=>{
-            const response = EmailService.sendCustomMail("s3945734658376e@gmail.com", "Test", "This is a test"); // <-- your code here
-            response.catch((err)=>{
-                err.should.be.exactly("Undefined recipient")
-            })
+            afterEach(async () => {
+                nodemailerMock.mock.reset();
+            });
+
+            after(async () => {
+                // Remove our mocked nodemailer and disable mockery
+                mockery.deregisterAll();
+                mockery.disable();
+            });
+
+            it('should send a number of student  email using nodemailer-mock', async () => {
+
+                const response = EmailService.sendStudentNumberEmail("s3945734658376e@gmail.com", "SE2", "13:00",3); // <-- your email code here
+                response.then((done) => {
+                    //console.log(done);
+                    done.response.should.be.exactly("nodemailer-mock success");
+                }).catch((err) => {
+                    console.log(err);
+                })
+            });
+
+            it('should fail to send an email using nodemailer-mock', async () => {
+                // tell the mock class to return an error
+                const err = new Error('My custom error');
+                nodemailerMock.mock.setShouldFailOnce();
+                nodemailerMock.mock.setFailResponse(err);
+
+                // call a service that uses nodemailer
+                const response = EmailService.sendStudentNumberEmail("fakeStudent.se2@gmail.com", "SE2", "13:00",3); // <-- your code here
+
+                // a test for our response
+                response.then((done) => {
+                    response.error.should.be.exactly(err);
+                })
+            });
+
+            it('should verify using the real nodemailer transport', async () => {
+                // tell the mock class to pass verify requests to nodemailer
+                nodemailerMock.mock.setMockedVerify(false);
+
+                // call a service that uses nodemailer
+                const response = EmailService.sendStudentNumberEmail("fakeStudent.se2@gmail.com", "SE2", "13:00",3); // <-- your code here
+
+                /* calls to transport.verify() will be passed through, 
+                transport.sendMail() is still mocked */
+                response.then((done) => {
+                    done.response.should.be.exactly("nodemailer-mock success");
+                }).catch((err) => {
+                    console.log(err);
+                })
+            });
+            it ("should verify that the email cannot send because the to is a nullish value", async()=>{
+                const response = EmailService.sendStudentNumberEmail(null, "SE2", "13:00",3); // <-- your code here
+                response.catch((err)=>{
+                    console.log(err);
+                    err.should.be.exactly("Undefined recipient")
+                })
+            });
+        })
+        describe('Tests EmailService.sendCustomEmail', async () => {
+            let app = null;
+
+            before(async () => {
+                // Enable mockery to mock objects
+                mockery.enable({
+                    warnOnUnregistered: false,
+                    useCleanCache: true,
+                });
+                mockery.registerMock('nodemailer', nodemailerMock);
+                EmailService = require('../src/services/EmailService');
+
+            });
+
+            afterEach(async () => {
+                nodemailerMock.mock.reset();
+            });
+
+            after(async () => {
+                // Remove our mocked nodemailer and disable mockery
+                mockery.deregisterAll();
+                mockery.disable();
+            });
+
+            it('should send a custom e-mail using nodemailer-mock', async () => {
+
+                const response = EmailService.sendCustomMail("s3945734658376e@gmail.com", "Test", "This is a test"); // <-- your email code here
+                response.then((done) => {
+                    //console.log(done);
+                    done.response.should.be.exactly("nodemailer-mock success");
+                }).catch((err) => { 
+                    console.log(err);
+                })
+            });
+
+            it('should fail to send a custom e-mail using nodemailer-mock', async () => {
+                // tell the mock class to return an error
+                const err = new Error('My custom error');
+                nodemailerMock.mock.setShouldFailOnce();
+                nodemailerMock.mock.setFailResponse(err);
+
+                // call a service that uses nodemailer
+                const response = EmailService.sendCustomMail("s3945734658376e@gmail.com", "Test", "This is a test"); // <-- your code here
+
+                // a test for our response
+                response.then((done) => {
+                    response.error.should.be.exactly(err);
+                })
+            });
+
+            it('should verify using the real nodemailer transport', async () => {
+                // tell the mock class to pass verify requests to nodemailer
+                nodemailerMock.mock.setMockedVerify(false);
+
+                // call a service that uses nodemailer
+                const response = EmailService.sendCustomMail("s3945734658376e@gmail.com", "Test", "This is a test"); // <-- your code here
+
+                /* calls to transport.verify() will be passed through, 
+                transport.sendMail() is still mocked */
+                response.then((done) => {
+                    done.response.should.be.exactly("nodemailer-mock success");
+                }).catch((err) => {
+                    console.log(err);
+                })
+            });
+            it ("should verify that the custom email cannot send because the to is a nullish value", async()=>{
+                const response = EmailService.sendCustomMail("s3945734658376e@gmail.com", "Test", "This is a test"); // <-- your code here
+                response.catch((err)=>{
+                    err.should.be.exactly("Undefined recipient")
+                })
+            });
         });
     });
 }
