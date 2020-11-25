85461ebafb294a96d357cea196ab0ed68f850207
paologastaldi-polito <s277393@studenti.polito.it>
2020-11-25 12:01:39 +0100

    fix: file paths

--- server/tests/Test_services.js
+++ server/tests/Test_services.js
@@ -4,9 +4,9 @@
  */
 'use strict';
 
-const Test_GeneralService = require('./Test_GeneralService');
-const Test_StudentService = require('./Test_StudentService');
-const Test_TeacherService = require('./Test_TeacherService');
+const Test_GeneralService = require('./Test_GeneralService.js');
+const Test_StudentService = require('./Test_StudentService.js');
+const Test_TeacherService = require('./Test_TeacherService.js');
 const Test_email = require('./Test_email.js');
 
 /**
