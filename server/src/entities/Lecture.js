b8e8d6cb46ca3af6592e3d11ca6298b8aa4214ad
Francesco Xia <s276509@studenti.polito.it>
2020-11-28 18:58:25 +0100

    feat: add create method
    the create method is a variant of from. It copies from the arguments
    only the attributes defined in the class you are copying in

--- server/src/entities/Lecture.js
+++ server/src/entities/Lecture.js
@@ -3,20 +3,28 @@
  * @author Gastaldi Paolo
  * @version 1.0.2
  */
-'use strict';
+"use strict";
 
 class Lecture {
     /**
      * constructor
-     * @param {Number} lectureId 
-     * @param {Number} courseId 
-     * @param {Number} classId 
-     * @param {Date} startingDate 
+     * @param {Number} lectureId
+     * @param {Number} courseId
+     * @param {Number} classId
+     * @param {Date} startingDate
      * @param {Number} duration - milliseconds
      * @param {Date} bookingDeadline
      * @param {DeliveryType} delivery
      */
-    constructor(lectureId = -1, courseId = -1, classId = -1, startingDate = undefined, duration = -1, bookingDeadline = undefined, delivery = Lecture.DeliveryType.ERROR) {
+    constructor(
+        lectureId = -1,
+        courseId = -1,
+        classId = -1,
+        startingDate = undefined,
+        duration = -1,
+        bookingDeadline = undefined,
+        delivery = Lecture.DeliveryType.ERROR
+    ) {
         startingDate = new Date(startingDate);
         bookingDeadline = new Date(bookingDeadline);
 
@@ -33,12 +41,12 @@ class Lecture {
      * how the lecture will be delivered
      */
     static DeliveryType = {
-        ERROR : 'ERROR',
-        UNDEFINED : 'UNDEFINED',
-        PRESENCE : 'PRESENCE',
-        REMOTE : 'REMOTE'
+        ERROR: "ERROR",
+        UNDEFINED: "UNDEFINED",
+        PRESENCE: "PRESENCE",
+        REMOTE: "REMOTE",
         // add more here
-    }
+    };
 
     /**
      * create a new lecture from a generic object
@@ -47,6 +55,21 @@ class Lecture {
      */
     static from(obj) {
         const lecture = Object.assign(new Lecture(), obj);
+        lecture.startingDate = new Date(lecture.startingDate);
+        lecture.bookingDeadline = new Date(lecture.bookingDeadline);
+        return lecture;
+    }
+
+    /**
+     * copy only existings properties from copyFrom
+     * @param {Object} copyFrom
+     * @returns {Lecture} new lecture
+     */
+    static create(copyFrom) {
+        const lecture = new Lecture();
+        Object.keys(copyFrom)
+            .filter((key) => key in lecture)
+            .forEach((key) => (lecture[key] = copyFrom[key]));
         lecture.startingDate = new Date(lecture.startingDate);
         lecture.bookingDeadline = new Date(lecture.bookingDeadline);
         return lecture;
