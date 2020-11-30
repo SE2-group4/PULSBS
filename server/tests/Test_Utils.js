eddaccc98abe5456890b8616a602958ecca18916
paologastaldi-polito <s277393@studenti.polito.it>
2020-11-30 15:05:43 +0100

    fix: utils test

--- server/tests/Test_Utils.js
+++ server/tests/Test_Utils.js
@@ -13,17 +13,20 @@ const utils = require('../src/utils/utils.js');
 const suite = function() {
     describe('Utils', function() {
         describe('toStandard', function() {
-            const errors = [
-                {
-                    errno : 0,
-                    location : 'test location',
-                    msg : 'test message',
-                    param : 'test param'
-                }
-            ];
+            const errors = {
+                errors : [
+                    {
+                        errno : 0,
+                        location : 'test location',
+                        msg : 'test message',
+                        param : 'test param'
+                    }
+                ]
+            };
 
             it('correct input should return specific error', function(done) {
                 const retError = utils.toStandard(errors, 999);
+                console.log(retError);
                 assert.strictEqual(typeof retError, typeof new utils.StandardErr(), 'Not a proper error');
                 assert.strictEqual(retError.statusCode, 999, 'Wrong error code');
                 done();
