24d35383df933eb21d803b153df586d78c3a9fc3
LorenzoCeccarelli <56482293+LorenzoCeccarelli@users.noreply.github.com>
2020-12-12 12:15:15 +0100

    [PULSBS-164] add new server APIs and update manager controller

--- server/src/controllers/ManagerController.js
+++ server/src/controllers/ManagerController.js
@@ -1,12 +1,15 @@
 const utils = require("../utils/writer");
 const express = require("express");
 const Manager = require("../services/ManagerService");
+const { Duplex } = require("nodemailer/lib/xoauth2");
 const router = express.Router();
 
 router.get("/:managerId/courses/:courseId/lectures/:lectureId", managerGetCourseLecture);
 router.get("/:managerId/courses/:courseId/lectures", managerGetCourseLectures);
 router.get("/:managerId/courses/:courseId", managerGetCourse);
 router.get("/:managerId/courses", managerGetCourses);
+router.get("/:managerId/students", managerGetStudent);
+router.get("/:managerId/tracingReport/:serialNumber", managerGetReport);
 module.exports.ManagerRouter = router;
 
 
@@ -54,3 +57,24 @@ function managerGetCourses(req, res) {
         });
 };
 module.exports.managerGetCourses = managerGetCourses;
+
+function managerGetStudent(req, res) {
+    Manager.managerGetStudent(req.params, req.query)
+        .then(function (response) {
+            utils.writeJson(res, response);
+        })
+        .catch(function (response) {
+            utils.writeJson(res, response);
+        });
+};
+module.exports.managerGetStudent = managerGetStudent;
+function managerGetReport(req, res) {
+    Manager.managerGetReport(req.params, req.query)
+        .then(function (response) {
+            utils.writeJson(res, response);
+        })
+        .catch(function (response) {
+            utils.writeJson(res, response);
+        });
+};
+module.exports.managerGetReport = managerGetReport;
