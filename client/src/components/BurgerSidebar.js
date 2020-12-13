416dd3a304d620d223c7d76645d73b160e3e1fdc
Alberto <pisanellof@hotmail.it>
2020-12-13 12:35:18 +0100

    [PULSBS-117] added daypicker and multiselect comp

--- client/src/components/BurgerSidebar.js
+++ client/src/components/BurgerSidebar.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { slide as Menu } from 'react-burger-menu'
+import DayPickerInput from 'react-day-picker/DayPickerInput';
+import MultiselectComp from '../components/MultiselectComp';
 
 class BurgerSidebar extends React.Component {
     constructor(props) {
@@ -10,10 +12,10 @@ class BurgerSidebar extends React.Component {
     render() {
         return (
             <Menu isOpen={true}>
-                <a id="from" className="menu-item" href="/">From</a>
-                <a id="to" className="menu-item" href="/about">To</a>
-                <a id="course" className="menu-item" href="/contact">Course</a>
-                <a id="type" className="menu-item" href="">Type</a>
+                <a id="from" className="menu-item" href="/">From <DayPickerInput /></a>
+                <a id="to" className="menu-item" href="/about">To <DayPickerInput /></a>
+                <a id="course" className="menu-item" href="/contact">Course <MultiselectComp></MultiselectComp></a>
+                <a id="type" className="menu-item" href="">Type <MultiselectComp></MultiselectComp></a>
             </Menu>
         )
     }
