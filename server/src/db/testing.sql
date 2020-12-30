61fb6bb1d2385670f69f2afd4840dcb97bb3e841
paologastaldi-polito <s277393@studenti.polito.it>
2020-12-30 23:47:31 +0100

    feat: new triggers for schedules

--- server/src/db/testing.sql
+++ server/src/db/testing.sql
@@ -78,10 +78,68 @@ INSERT INTO Calendar(calendarId, startingDate, endingDate, type, isAValidPeriod)
 INSERT INTO Calendar(calendarId, startingDate, endingDate, type, isAValidPeriod) VALUES(4, DATETIME('now', '+7 day', 'start of day'), DATETIME('now', '+14 day', 'start of day'), 'HOLIDAYS', 0);
 INSERT INTO Calendar(calendarId, startingDate, endingDate, type, isAValidPeriod) VALUES(5, DATETIME('now', '+30 dau', 'start of day'), DATETIME('now', '+32 day', 'start of day'), 'HOLIDAYS', 0);
 
-DROP TRIGGER IF EXISTS check_time_overlapping_before_insert_lecture;
-CREATE TRIGGER check_time_overlapping_before_insert_lecture BEFORE INSERT ON Lecture BEGIN SELECT CASE WHEN ( SELECT COUNT(*) <> 0 FROM Lecture WHERE DATETIME(NEW.startingDate) <= DATETIME(startingDate, '+' || duration || ' millisecond') AND DATETIME(NEW.startingDate, '+' || NEW.duration || ' millisecond') >= DATETIME(startingDate) ) THEN RAISE(ABORT, 'New lecture overlapped with an existing one') END IF; END;
-DROP TRIGGER IF EXISTS check_time_overlapping_before_update_lecture;
-CREATE TRIGGER check_time_overlapping_before_update_lecture BEFORE UPDATE ON Lecture BEGIN SELECT CASE WHEN ( SELECT COUNT(*) <> 0 FROM Lecture WHERE DATETIME(NEW.startingDate) <= DATETIME(startingDate, '+' || duration || ' millisecond') AND DATETIME(NEW.startingDate, '+' || NEW.duration || ' millisecond') >= DATETIME(startingDate) ) THEN RAISE(ABORT, 'New lecture overlapped with an existing one') END IF; END;
+DROP TRIGGER IF EXISTS check_time_overlapping_before_insert_schedule;
+CREATE TRIGGER check_time_overlapping_before_insert_schedule
+	BEFORE INSERT ON Schedule
+	BEGIN
+		SELECT CASE WHEN ( SELECT COUNT(*) <> 0
+			FROM Shedule
+			WHERE
+				NEW.code = code
+				AND DATETIME(NEW.startingTime) <= DATETIME(endingTime)
+				AND DATETIME(NEW.endingTime) >= DATETIME(startingTime) )
+			THEN RAISE(ABORT, 'New schedule overlapped with an existing one with the same code')
+		END IF;
+		SELECT CASE WHEN ( SELECT COUNT(*) <> 0
+			FROM Schedule
+			WHERE
+				NEW.code <> code
+				AND NEW.roomId = roomId
+				AND DATETIME(NEW.startingTime) <= DATETIME(endingTime)
+				AND DATETIME(NEW.endingTime) >= DATETIME(startingTime) )
+			THEN RAISE(ABORT, 'New schedule overlapped with an existing one in the same class')
+		END IF;
+	END;
+
+DROP TRIGGER IF EXISTS check_time_overlapping_before_update_schedule;
+CREATE TRIGGER check_time_overlapping_before_update_schedule
+	BEFORE UPDATE ON Schedule
+	BEGIN
+		SELECT CASE WHEN (
+            SELECT COUNT(*) <> 0 FROM Shedule
+                WHERE NEW.code = code
+                    AND DATETIME(NEW.startingTime) <= DATETIME(endingTime)
+                    AND DATETIME(NEW.endingTime) >= DATETIME(startingTime) )
+			THEN RAISE(ABORT, 'New schedule overlapped with an existing one with the same code')
+		END IF;
+		SELECT CASE WHEN (
+            SELECT COUNT(*) <> 0 FROM Schedule
+                WHERE NEW.code <> code
+                    AND NEW.roomId = roomId
+                    AND DATETIME(NEW.startingTime) <= DATETIME(endingTime)
+                    AND DATETIME(NEW.endingTime) >= DATETIME(startingTime) )
+			THEN RAISE(ABORT, 'New schedule overlapped with an existing one in the same class')
+		END IF;
+	END;
 
 DROP trigger IF EXISTS delete_bookings_after_delete_lecture;
-CREATE TRIGGER delete_bookings_after_delete_lecture BEFORE DELETE ON Lecture BEGIN INSERT INTO EmailQueue(sender, recipient, emailType, teacherId, studentId, courseId, courseName, lectureId, startingDate) SELECT TempLecture.email, User.email, "LESSON_CANCELLED", TempLecture.teacherId, Booking.studentId, TempLecture.courseId, TempLecture.description, TempLecture.lectureId, TempLecture.startingDate FROM Booking, (SELECT * FROM Lecture, Course, TeacherCourse, User WHERE Lecture.lectureId = OLD.lectureId AND Lecture.courseId = TeacherCourse.courseId AND TeacherCourse.teacherId = User.userId AND Course.courseId = Lecture.courseId) AS TempLecture, User WHERE Booking.lectureId = OLD.lectureId AND Booking.studentId = User.userId AND Booking.lectureId = TempLecture.lectureId; DELETE FROM Booking WHERE Booking.lectureId = OLD.lectureId; DELETE FROM WaitingList WHERE WaitingList.lectureId = OLD.lectureId; END;
+CREATE TRIGGER delete_bookings_after_delete_lecture
+    BEFORE DELETE ON Lecture
+    BEGIN
+        INSERT INTO EmailQueue(sender, recipient, emailType, teacherId, studentId, courseId, courseName, lectureId, startingDate)
+            SELECT TempLecture.email, User.email, "LESSON_CANCELLED", TempLecture.teacherId, Booking.studentId, TempLecture.courseId, TempLecture.description, TempLecture.lectureId, TempLecture.startingDate
+                FROM Booking, (
+                    SELECT * FROM Lecture, Course, TeacherCourse, User
+                        WHERE Lecture.lectureId = OLD.lectureId
+                            AND Lecture.courseId = TeacherCourse.courseId
+                            AND TeacherCourse.teacherId = User.userId
+                            AND Course.courseId = Lecture.courseId
+                    ) AS TempLecture, User
+                WHERE Booking.lectureId = OLD.lectureId
+                    AND Booking.studentId = User.userId
+                    AND Booking.lectureId = TempLecture.lectureId;
+        DELETE FROM Booking
+            WHERE Booking.lectureId = OLD.lectureId;
+        DELETE FROM WaitingList
+            WHERE WaitingList.lectureId = OLD.lectureId;
+    END;
